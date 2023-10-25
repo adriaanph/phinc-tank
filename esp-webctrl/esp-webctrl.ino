7dbd9692bc3f6b6a24c600341ccf848f7f9f0764
adriaanph <adriaan.peenshough@gmail.com>
2023-10-26 08:59:48 +1300

    Added example for pin input & output.

--- esp-webctrl/esp-webctrl.ino
+++ esp-webctrl/esp-webctrl.ino
@@ -7,18 +7,23 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 
+// Some input & output pins for demonstration
+// ADC2 pins cannot be used when Wi-Fi is used, so can only use pins 32 - 39 [https://randomnerdtutorials.com/esp32-pinout-reference-gpios]
+#define OUT_PIN 23
+#define IN_PIN 34
+
 // These details are used first for the ESP to attempt joining an existing WiFi network
 #define JOIN_WIFI_ID "existing_wifi_ssid"
 #define JOIN_WIFI_PSWD "existing_wifi_password"
 // These details are used if the ESP needs to create a stand-alone WiFi access point for clients to connect to
-#define SERVER_ID "esp-control" // WiFi SSID as well as web server name
-#define SERVER_PSWD NULL // No password required to connect!
+#define WIFI_ID "esp-control" // WiFi SSID as well as web server name
+#define WIFI_PSWD NULL // No password required to connect!
 #define WIFI_CHANNEL 1 // Static channel, hopefully not congested...
 
 
 ESP8266WebServer ui_webserver(80); // HTTP (web) server on default port
 
-char cmd; // Command received by input handler in current cycle
+String cmd; // Command received by input handler in current cycle
 
 
 void ui_webserver_init() {
@@ -68,32 +73,32 @@ void ui_webserver_display() {
   // timeout=100millisec is my workaround for bug <https://www.tablix.org/~avian/blog/archives/2022/08/esp8266_web_server_is_slow_to_close_connections> - timouts in browser console are harmless
   // Commands:
   script += "function send(cmd) {";
-  script += "var req = new XMLHttpRequest(); req.open(\"POST\", \"cmd?\"+cmd); req.timeout=100; req.send(null);";
+  script += "var req = new XMLHttpRequest(); req.open(\"POST\", \"cmd?\"+cmd+\"=1\"); req.timeout=100; req.send(null);";
   script += "}";
   // Queries: there must be an HTML element with ID==query and it must have an explicit 'close' tag!
   script += "function retrv(query) {";
   script += "var req = new XMLHttpRequest(); req.onreadystatechange = function() {";
   script += "  if (this.readyState==4 && this.status==200) { x=document.getElementById(query); if (x!=null) x.innerHTML=this.responseText; }";
-  script += "  }; req.open(\"GET\", \"query?\"+query); req.timeout=100; req.send(null);";
+  script += "  }; req.open(\"GET\", \"query?\"+query+\"=1\"); req.timeout=100; req.send(null);";
   script += "}</script>";
 
   String style = "<head><style>";
   style += "body {font-size:55px;}";
-  style += "button {font-size:55px; line-height:155px}"; // Big buttons
+  style += "button {font-size:55px; line-height:155px}"; // Default for all buttons
   style += ".panel {line-height:200px; text-align:center; border:10px outset lightblue; border-radius:40px;}"; // Lot of space between buttons
   style += "td {text-align:center;}"; // Default table cell alignment
   style += "</style></head>";
   
   String body = "<body><h1 align=\"center\">Control Panel</h1>";
   body += "<div class=\"panel\"><table align=\"center\">";
-  body += "<tr> <td/> <td><button onclick=\"send('W');\">FWD</button></td> <td/> </tr>";
-  body += "<tr> <td><button onclick=\"send('A');\">LEFT</button></td> <td><button onclick=\"send('S');\">STOP</button></td> <td><button onclick=\"send('D');\">RIGHT</button></td> </tr>";
+  body += "<tr> <td/> <td><button onclick=\"send('OUT=1');\">ON</button></td> <td/> <td><button onclick=\"send('OUT=0');\">OFF</button></td> </tr>";
   body += "<tr/>";
-  body += "<tr> <td><button onclick=\"retrv('M');\">GET M</button></td> <td id='M'></td> <td id='T'></td> </tr>";
+  body += "<tr> <td><button onclick=\"retrv('INmV');\">GET IN</button></td> <td/> <td id='INmV'></td> <td>mV</td> </tr>";
+  body += "<tr> <td/> <td/> <td>Uptime</td> <td id='UPT'></td> </tr>";
   body += "</table></div></body>";
   
   // Queries that are run at regular intervals
-  String auto_update = "<script type=\"text/javascript\">setInterval(function(){retrv('T');}, 2000);</script>";
+  String auto_update = "<script type=\"text/javascript\">setInterval(function(){retrv('UPT');}, 2000);</script>";
 
   ui_webserver.send(200, "text/html", script + style + body + auto_update);
 }
@@ -101,10 +106,16 @@ void ui_webserver_display() {
 void ui_webserver_cmd() {
   /** Handles command requests, doesn't send anything back to change the interface. */
   if (ui_webserver.args() == 1) {
-    cmd = ui_webserver.argName(0)[0]; // Change this to work with more than single character inputs
-    // Sample code to do something with the command
+    cmd = ui_webserver.argName(0);
+    String val = ui_webserver.arg(0);
+    // Code to do something with the command
     Serial.print("Got command: "); Serial.println(cmd);
-    if (cmd == 'W') {
+    if (cmd == "OUT") {
+      if (val[0] == '0') {
+        digitalWrite(OUT_PIN, 0);
+      } else if (val[0] == '1') {
+        digitalWrite(OUT_PIN, 1);
+      }
     }
   }
 }
@@ -112,26 +123,38 @@ void ui_webserver_cmd() {
 void ui_webserver_query() {
   /** Handles query requests, sends plain text back for browser script to update the interface (no page reload). */
   if (ui_webserver.args() == 1) {
-    char query = ui_webserver.argName(0)[0]; // Change this to work with more than single character inputs
-    // Sample code to respond to query
+    String query = ui_webserver.argName(0);
+    // Code to respond to query
     Serial.print("Got query: "); Serial.println(query);
-    if (query == 'T') {
+    if (query == "UPT") {
       ui_webserver.send(200, "text/plain", String(millis()));
-    } else {
-      ui_webserver.send(200, "text/plain", "OK!");
+    } else if (query == "INmV") {
+      int mV = analogReadMilliVolts(IN_PIN);
+      ui_webserver.send(200, "text/plain", String(mV));
     }
   }
 }
 
 
+void handle_internals() {
+  /** Things that need to happen once per loop, regardless of user input. */
+  // NB: this must not take more than ~ 100ms!
+}
+
+
 void setup() {
+  // Enable debugging via serial monitor
   Serial.begin(9600);
+  // Configure the web server for User Interface
   ui_webserver_init();
+  // Configure the input & outputs
+  pinMode(IN_PIN, INPUT);
+  pinMode(OUT_PIN, OUTPUT);
 }
 
 
 void loop() {
-  cmd = 0;
+  cmd = "";
   ui_webserver.handleClient();
-  // Prefer to only act on commands in "ui_webserver_cmd", not here
+  handle_internals();
 }
